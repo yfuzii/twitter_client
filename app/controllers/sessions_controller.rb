e96430644f95e02078f1fcff390f48e6978a3eae
Yuki <apijufy@gmail.com>
2018-12-03 23:07:17 +0900

    #05 タイムライン取得機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     user = User.find_or_create_from_auth_hash(auth)
     session[:user_id] = user.id
+    session[:oauth_token] = auth['credentials']['token']
+    session[:oauth_token_secret] = auth['credentials']['secret']
     user.increment!(:login_count)
     flash[:success] = 'ログインしました。'
     redirect_to root_path
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,22 @@
 class ToppagesController < ApplicationController
+  before_action :client_new
+
   def home
-    @user = current_user if current_user
+    if current_user
+      @user = current_user
+      @tweets = @client.home_timeline(include_entitles: true)
+    end
   end
+
+  private
+
+    # OAuth認証の設定
+    def client_new
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key = Rails.application.secrets.twitter_api_key
+        config.consumer_secret = Rails.application.secrets.twitter_api_secret
+        config.access_token = session[:oauth_token]
+        config.access_token_secret = session[:oauth_token_secret]
+      end
+    end
 end
