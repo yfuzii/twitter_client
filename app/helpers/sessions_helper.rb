16be0803d7b4bc61032c990529d832294f3cf684
Yuki <apijufy@gmail.com>
2018-12-04 09:15:38 +0900

    #07 レコメンドユーザーの表示

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+  # ログインしているユーザを返す
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # 初回ログインの判定
+  def first_login?
+    current_user.login_count <= 1
+  end
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,22 @@
+module Constants
+  RECOMMENDED_USERS = [
+    {
+      id: 0,
+      name: "jack",
+      screen_name: "jack",
+      image_url: "https://pbs.twimg.com/profile_images/839863609345794048/mkpdB9Tf_normal.jpg"
+    },
+    {
+      id: 1,
+      name: "Yukihiro Matsumoto",
+      screen_name: "yukihiro_matz",
+      image_url: "https://pbs.twimg.com/profile_images/511244255294001152/v5phEU1O_normal.jpeg"
+    },
+    {
+      id: 2,
+      name: "Jeff Bezos",
+      screen_name: "JeffBezos",
+      image_url: "https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_normal.jpg"
+    }
+  ] 
+end
